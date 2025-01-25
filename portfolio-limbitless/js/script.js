813791205693093fec2049db1614bdbe34588222
lily <164814255+trogoodkoe@users.noreply.github.com>
2025-01-25 17:17:36 -0500

    working on about page

--- portfolio-limbitless/js/script.js
+++ portfolio-limbitless/js/script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector('.typing-text').style.animation = 'typing 2s steps(10, end) forwards';
+    }, 1000); // 1 second delay
+});
+
 <script src="https://kit.fontawesome.com/efb49abce0.js" crossorigin="anonymous"></script>
