472e23fb0562a3d36e059ab74bc203727361d363
Lily Vrionis <lsvrionis@gmail.com>
2025-06-04 16:08:47 -0400

    setting up

--- portfolio-limbitless/lsvrionis-portfolio/js/script.js
+++ portfolio-limbitless/lsvrionis-portfolio/js/script.js
@@ -1,7 +1,30 @@
+
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         document.querySelector('.typing-text').style.animation = 'typing 2s steps(10, end) forwards';
     }, 1000); // 1 second delay
 });
 
-<script src="https://kit.fontawesome.com/efb49abce0.js" crossorigin="anonymous"></script>
+const skillItems = document.querySelectorAll('.skill-item');
+
+skillItems.forEach((skillItem) => {
+  skillItem.addEventListener('click', () => {
+    console.log(`Activated skill item: ${skillItem.getAttribute('title')}`);
+  });
+});
+
+skillItems.forEach((skillItem) => {
+    skillItem.addEventListener('click', (event) => {
+      if (event.type === 'click' || event.type === 'touchstart') {
+        console.log(`Activated skill item: ${skillItem.getAttribute('title')}`);
+      }
+    });
+  });
+
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerMenu.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter' || event.key === ' ') {
+    // toggle the hamburger menu
+  }
+});
